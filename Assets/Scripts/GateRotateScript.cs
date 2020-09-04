b737a80365492577715da60a7b62f818d37c4c0a
rose-water <rachel.ulgado@gmail.com>
2020-09-04 11:03:28 -0700

    initial commit

---
+++ Assets/Scripts/GateRotateScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GateRotateScript : MonoBehaviour {
+  private float turnSpeed = 1.0f;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# IOET MDP Summer Research
+### Ben Hooker, Martin Bernard, Rachel Rose Waterhouse
+
+---
+Uses Unity version 2019.2.9f1
