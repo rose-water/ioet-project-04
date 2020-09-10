5b479e178d4a9099094d1a2fb2783cd49b1f3c41
Martin Bernard <mwbernard9@gmail.com>
2020-09-10 11:19:57 -0700

    adds scene 2 with 'houses' and fade in out script

---
+++ Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    public float fadeInSpeed;
+    public float fadeOutSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Renderer>().material.color.a > .95) {
+            StartCoroutine(FadeOutObject());
+        } else if (GetComponent<Renderer>().material.color.a < .05){
+            StartCoroutine(FadeInObject());
+        }
+
+
+    }
+
+    public IEnumerator FadeInObject() {
+        while (GetComponent<Renderer>().material.color.a < 1) {
+            Color objectColor = GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a + (fadeInSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            GetComponent<Renderer>().material.color = objectColor;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOutObject() {
+        while (GetComponent<Renderer>().material.color.a > 0) {
+            Color objectColor = GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeOutSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            GetComponent<Renderer>().material.color = objectColor;
+            yield return null;
+        }
+    }
+
+
+}
